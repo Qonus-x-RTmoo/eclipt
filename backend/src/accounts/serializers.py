4c667a576bc9be9d7025e13e26f7ecb8e15d06fa
RTMoo <toktarkazyramazan@gmail.com>
2025-05-09 16:48:17 +0500

    feat: Регистрация, Аутентификация, Подтверждение почты и докер

---
+++ backend/src/accounts/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework.serializers import (
+    ModelSerializer,
+    EmailField,
+    CharField,
+    ValidationError,
+)
+from accounts.models import CustomUser
+from accounts.tasks import send_confirmation_email
+
+
+
+class UserRegistrationSerializer(ModelSerializer):
+    """
+    Сериализатор для регистрации пользователя.
+    """
+
+    email = EmailField(required=True)
+    username = CharField()
+    password = CharField(write_only=True)
+    password2 = CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ["email", "username", "password", "password2"]
+
+    def validate(self, data):
+        """
+        Проверяет, существует ли пользователь с таким email, и совпадают ли пароли.
+        """
+
+        if CustomUser.objects.only("id").filter(email=data["email"]).exists():
+            raise ValidationError({"email": "Этот email уже используется"})
+
+        if data["password"] != data["password2"]:
+            raise ValidationError({"password": "Пароли не совпадают"})
+
+        return data
+
+    def create(self, validated_data):
+        """
+        Создаёт нового пользователя после успешной валидации данных.
+        """
+
+        validated_data.pop("password2")
+        user = CustomUser.objects.create_user(**validated_data)
+
+        send_confirmation_email.delay(user.email)
+        return user
--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+from datetime import timedelta
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -16,7 +17,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -28,6 +29,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
+    "drf_spectacular",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -44,7 +50,21 @@ ROOT_URLCONF = "config.urls"
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
@@ -67,8 +87,76 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "accounts.authentication.CookieJWTAuthentication",
+    ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Название вашего API",
+    "DESCRIPTION": "Подробное описание вашего API",
+    "VERSION": "1.0.0",
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "AUTH_COOKIE": "access_token",
+    "AUTH_COOKIE_SECURE": False,  # True для Production
+    "AUTH_COOKIE_HTTP_ONLY": True,
+    "AUTH_COOKIE_SAMESITE": "Lax",
+}
+
+# CORS
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_METHODS = ["GET", "POST", "PATCH", "DELETE"]
+CORS_ALLOW_HEADERS = ["Authorization", "Content-Type"]
+CORS_MAX_AGE = 600  # Кэшировать preflight на 10 минут
+CORS_ALLOW_CREDENTIALS = True  # Разрешает передавать куки и токены
+
+# Только для локального
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+# Redis
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
+# Celery
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+
+EMAIL_BACKENDS = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
