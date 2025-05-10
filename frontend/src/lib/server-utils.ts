5fc2a20fa0ad824498d4d5c485289128a44cff06
Qonus <94901082+Qonus@users.noreply.github.com>
2025-05-10 21:28:31 +0500

    Create server-utils.ts

---
+++ frontend/src/lib/server-utils.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { headers } from "next/headers";
+import { userAgent } from "next/server";
+
+export async function getIsMobile() {
+    const { device } = userAgent({ headers: await headers() })
+    const isMobile = device?.type === 'mobile'
+    return isMobile;
+}
+
+export async function getApiUrl() {
+    return process.env.API_URL;
+}
+
+export async function getBaseUrl() {
+    const headersList = await headers();
+    const host = headersList.get('host');
+    const protocol = headersList.get('x-forwarded-proto') || 'http';
+    const baseUrl = `${protocol}://${host}`;
+    return baseUrl;
+}
